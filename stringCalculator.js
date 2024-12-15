7f5b10044469cc35192167ff13601ecc01269a46
USER <ketanwalunjkar12@gmail.com>
2024-12-15 15:16:36 +0530

    Handle New Lines

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,8 +1,7 @@
 function add(numbers) {
     if (numbers === "") return 0;
-    if (!numbers.includes(",")) return parseInt(numbers, 10);
 
-    const numArray = numbers.split(",").map(num => parseInt(num, 10));
+    const numArray = numbers.split(/[\n,]/).map(num => parseInt(num, 10));
     return numArray.reduce((sum, num) => sum + num, 0);
 }
 
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -11,3 +11,8 @@ test('returns the number itself if the input contains a single number', () => {
 test('returns the sum of two numbers separated by a comma', () => {
     expect(stringCalculator.add("1,5")).toBe(6);
 });
+
+test('handles new lines as delimiters in addition to commas', () => {
+    expect(stringCalculator.add("1\n2,3")).toBe(6);
+});
+
