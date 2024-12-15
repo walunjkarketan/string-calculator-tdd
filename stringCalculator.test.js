e3e14be3ff9ee69d3edb73405f7dbb7a998871a9
USER <ketanwalunjkar12@gmail.com>
2024-12-15 15:01:57 +0530

    Handle Single Number, updated test case

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -1,5 +1,9 @@
 const stringCalculator = require('./stringCalculator');
 
+test('returns 0 for an empty string', () => {
+    expect(stringCalculator.add("")).toBe(0);
+});
+
 test('returns the number itself if the input contains a single number', () => {
     expect(stringCalculator.add("1")).toBe(1);
 });
