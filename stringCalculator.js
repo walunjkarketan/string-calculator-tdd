361486e4058bc0bff565f0c1941d57cd97e1d0b1
USER <ketanwalunjkar12@gmail.com>
2024-12-15 15:08:15 +0530

    Handle Two Numbers

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,8 +1,9 @@
 function add(numbers) {
     if (numbers === "") return 0;
     if (!numbers.includes(",")) return parseInt(numbers, 10);
-    return null;
-}
 
+    const numArray = numbers.split(",").map(num => parseInt(num, 10));
+    return numArray.reduce((sum, num) => sum + num, 0);
+}
 
 module.exports = { add };
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -8,3 +8,6 @@ test('returns the number itself if the input contains a single number', () => {
     expect(stringCalculator.add("1")).toBe(1);
 });
 
+test('returns the sum of two numbers separated by a comma', () => {
+    expect(stringCalculator.add("1,5")).toBe(6);
+});
